d52721715849438cb21706c64e29655f29360326
firefox1234123 <dayun01004@gmail.com>
2024-11-23 22:51:14 +0900

    18주차 과제

---
+++ dayeon/likelion-testphone/src/main/java/com/likelion/likeliontestphone/LikelionTestphoneApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.likeliontestphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikelionTestphoneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikelionTestphoneApplication.class, args);
+    }
+
+}
